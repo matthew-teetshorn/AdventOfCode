e30e91c4a22eda314fe2d89ec357088a8bc1a088
matthew-teetshorn <matthew.teetshorn@gmail.com>
2025-12-09 15:48:23 -0600

    Day 9 part 1

---
+++ 2025/Day9/ex9a.c
@@ -0,0 +1,48 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define DIM 11
+#define NUM_POINTS 495
+
+typedef struct {
+  int x;
+  int y;
+} Coord_t;
+
+int main() {
+  FILE *fp = fopen("ex9.input", "r");
+  if (fp == NULL) {
+    printf("Error opening file\n");
+    return EXIT_FAILURE;
+  }
+
+  char buffer[DIM + 2];
+  Coord_t coords[NUM_POINTS];
+
+  for (int i = 0; i < NUM_POINTS; i++) {
+    if (fgets(buffer, DIM + 2, fp) == NULL) {
+      printf("Error reading from file to buffer\n");
+      return EXIT_FAILURE;
+    }
+
+    sscanf(buffer, "%d,%d", &coords[i].x, &coords[i].y);
+  }
+
+  uint64_t currArea;
+  uint64_t maxArea = 0;
+  for (int i = 0; i < NUM_POINTS; i++) {
+    for (int j = i + 1; j < NUM_POINTS; j++) {
+      Coord_t p1 = {coords[i].x, coords[i].y};
+      Coord_t p2 = {coords[j].x, coords[j].y};
+      currArea =
+          (uint64_t)(abs(p1.x - p2.x) + 1) * (uint64_t)(abs(p1.y - p2.y) + 1);
+      if (currArea > maxArea) {
+        maxArea = currArea;
+      }
+    }
+  }
+
+  printf("Max area: %" PRIu64 "\n", maxArea);
+}
