b9891f35ff03ee62e82b2d17395364519d00a1cb
matthew-teetshorn <matthew.teetshorn@gmail.com>
2026-01-10 12:42:37 -0600

    Day 14 part 1

---
+++ 2016/Day14/ex14a.py
@@ -0,0 +1,42 @@
+import hashlib
+from functools import lru_cache
+
+
+@lru_cache(maxsize=1000)
+def gen_hash(key: str) -> str:
+    hash = hashlib.md5()
+    hash.update(key.encode())
+    return hash.hexdigest()
+
+
+def scan_ahead(char: str, salt: str, start: int) -> bool:
+    for index in range(start, start + 1000):
+        hash = gen_hash(salt + str(index))
+        for a, b, c, d, e in zip(hash, hash[1:], hash[2:], hash[3:], hash[4:]):
+            if char == a == b == c == d == e:
+                return True
+
+    return False
+
+
+def main():
+    with open("ex14.input", "r") as f:
+        input = f.read().strip()
+
+    key_count = 0
+    index = 0
+    while key_count < 64:
+        index += 1
+        curr_key = input + str(index)
+        hash = gen_hash(curr_key)
+        for x, y, z in zip(hash, hash[1:], hash[2:]):
+            if x == y == z:
+                if scan_ahead(x, input, index + 1):
+                    key_count += 1
+                break
+
+    print(f"Index of final key found: {index}")
+
+
+if __name__ == "__main__":
+    main()
